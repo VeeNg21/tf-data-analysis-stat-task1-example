d7faab20e90b7fd54cd28f4ab98e86ed6f148639
Vee21 <55953389+VeeNg21@users.noreply.github.com>
2023-04-04 13:12:19 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,9 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1374771107 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    N = len(x)
+    a = (1 / N) * np.sum(np.log(x)) - (1 / (2 * N)) * np.sum(np.log(x))**2
+    return a
