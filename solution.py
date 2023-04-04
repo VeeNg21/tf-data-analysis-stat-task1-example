4b507b04cc76c209771fe6ae580b8f5376ffebf8
Vee21 <55953389+VeeNg21@users.noreply.github.com>
2023-04-04 14:06:51 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import lognorm
 
 chat_id = 1374771107 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    N = len(x)
-    a = (1 / N) * np.sum(np.log(x)) - (1 / (2 * N)) * np.sum(np.log(x))**2
+    shape, loc, scale = lognorm.fit(x, floc=0)
+    a = np.exp(loc)
     return a
